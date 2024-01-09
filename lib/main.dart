b7d3fbce238beb5f5faf8a709286c90509653f94
FelipeJorge805 <feliperkj805@gmail.com>
2024-01-09 16:30:16 -0500

    Added dismissible current list with save button icon,
    Confirm alert dialog when attemptying to delete entire current list

--- lib/main.dart
+++ lib/main.dart
@@ -258,7 +258,53 @@ class ListPage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
 
     return ListTile(
-        title: const Text("Shopping list"),
+        /*leading: //save button
+          IconButton(
+            icon: const Icon(Icons.save),
+            onPressed: () => {appState.addCurrentListToHistory(), appState.shoppingList.clear(),}
+          ),*/
+        title: Dismissible(
+          key: UniqueKey(),
+          background: Container(
+            alignment: AlignmentDirectional.centerStart,
+            color: Colors.red,
+            child: const Icon(Icons.cancel),
+          ),
+          secondaryBackground: Container(
+            alignment: AlignmentDirectional.centerEnd,
+            color: Colors.red,
+            child: const Icon(Icons.cancel),
+          ),
+          onDismissed: (direction) => {appState.shoppingList.clear(),},
+          confirmDismiss:(direction) => showDialog<bool>(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text("Clear list?"),
+                content: const Text("Are you sure you want to clear the entire list?\nYou can save it instead by tapping instead of swiping."),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(false),
+                    child: const Text("Cancel"),
+                  ),
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(true),
+                    child: const Text("Clear"),
+                  ),
+                ],
+              );
+            },
+          ),
+          child: TextButton.icon(
+            onPressed: () => {
+              appState.addCurrentListToHistory(),
+              appState.shoppingList.clear(),
+            },
+            label: const Text('Current list'),
+            icon: //save icon
+              const Icon(Icons.save_alt),
+          )
+        ),
         subtitle: ListView(
         children: [
           for (var item in appState.shoppingList) 
