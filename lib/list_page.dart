ba0434620ce8b5bda1284904faeb894c36c63240
FelipeJorge805 <feliperkj805@gmail.com>
2024-01-24 12:29:19 -0500

    Removed another FileStorage use

--- lib/list_page.dart
+++ lib/list_page.dart
@@ -11,48 +11,47 @@ class ListPage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
 
     return ListTile(
-        /*leading: //save button
-          IconButton(
-            icon: const Icon(Icons.save),
-            onPressed: () => {appState.addCurrentListToHistory(), appState.shoppingList.clear(),}
-          ),*/
-        title: Dismissible(
-          key: UniqueKey(),
-          background: Container(
-            alignment: AlignmentDirectional.centerStart,
-            color: Colors.red,
-            child: const Icon(Icons.cancel),
-          ),
-          secondaryBackground: Container(
-            alignment: AlignmentDirectional.centerEnd,
-            color: Colors.red,
-            child: const Icon(Icons.cancel),
-          ),
-          onDismissed: (direction) => {     //clear current list button
-              appState.shoppingList.clear(),
-              FileStorage().saveDataToFile('current.txt', ''),
-            },
-          confirmDismiss:(direction) => showDialog<bool>(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                title: const Text("Clear list?"),
-                content: const Text("Are you sure you want to clear the entire list?\nYou can save it instead by tapping instead of swiping."),
-                actions: <Widget>[
-                  TextButton(
-                    onPressed: () => Navigator.of(context).pop(false),
-                    child: const Text("Cancel"),
-                  ),
-                  TextButton(
-                    onPressed: () => Navigator.of(context).pop(true),
-                    child: const Text("Clear"),
-                  ),
-                ],
-              );
-            },
-          ),
-          child: TextButton.icon(
-            onPressed: () => {  //save current list button
+      /*leading: //save button
+        IconButton(
+          icon: const Icon(Icons.save),
+          onPressed: () => {appState.addCurrentListToHistory(), appState.shoppingList.clear(),}
+        ),*/
+      title: Dismissible(
+        key: UniqueKey(),
+        background: Container(
+          alignment: AlignmentDirectional.centerStart,
+          color: Colors.red,
+          child: const Icon(Icons.cancel),
+        ),
+        secondaryBackground: Container(
+          alignment: AlignmentDirectional.centerEnd,
+          color: Colors.red,
+          child: const Icon(Icons.cancel),
+        ),
+        onDismissed: (direction) => {     //clear current list button
+            appState.clearShoppingList(),
+          },
+        confirmDismiss:(direction) => showDialog<bool>(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text("Clear list?"),
+              content: const Text("Are you sure you want to clear the entire list?\nYou can save it instead by tapping instead of swiping."),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(false),
+                  child: const Text("Cancel"),
+                ),
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(true),
+                  child: const Text("Clear"),
+                ),
+              ],
+            );
+          },
+        ),
+        child: TextButton.icon(
+          onPressed: () => {  //save current list button
             appState.clearShoppingList(),
             appState.addCurrentListToHistory(),
           },
